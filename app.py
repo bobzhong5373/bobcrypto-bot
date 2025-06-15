6c55b6b844eb425652d109b367b06bcac3b6450a
bobzhong5373 <bobzhong5273@gmail.com>
2025-06-15 10:58:15 +0800

    更新 app.py

--- app.py
+++ app.py
@@ -27,4 +27,5 @@ def send_message(chat_id, text):
     requests.post(url, json={"chat_id": chat_id, "text": text})
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
